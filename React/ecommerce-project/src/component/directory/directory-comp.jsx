f6db31d42a2e3d2e7a4e376afbdca102ad93c703
=avihaySunny <=avihay.sunny@gmail.com>
2020-01-06 15:05:12 +0200

    components react

---
+++ React/ecommerce-project/src/component/directory/directory-comp.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import MenuItem from '../menu-item/menu-item-comp'
+import {sections} from './directory.data'
+import './directory-comp-styles.scss'
+
+export class Directory extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            sections
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({title, imageUrl, id, size}) => (
+                        <MenuItem key = {id} title = {title} imageUrl ={imageUrl} size = {size}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory
