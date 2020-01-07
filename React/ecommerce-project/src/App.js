864cf21e829c36fb2aa923df6c436bc4002db1a7
=avihaySunny <=avihay.sunny@gmail.com>
2020-01-07 11:52:21 +0200

    shop page done

--- React/ecommerce-project/src/App.js
+++ React/ecommerce-project/src/App.js
@@ -3,11 +3,14 @@ import {Switch, Route} from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage-comp'
 import ShopPage from './pages/shoppage/shop-comp'
+import Header from './component/header/header-comp'
+
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>  
         <Route exact path='/shop' component={ShopPage}/>
