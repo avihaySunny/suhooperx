fc3876d7c468dc3abee0303db1b092782f2f666a
=avihaySunny <=avihay.sunny@gmail.com>
2020-01-08 16:32:12 +0200

    redirection

--- React/ecommerce-project/src/App.js
+++ React/ecommerce-project/src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import {connect} from 'react-redux' 
 
 import HomePage from './pages/homepage/homepage-comp'
@@ -47,13 +47,19 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>  
           <Route exact path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' component={SignInPage}/>
+          <Route exact path='/signin' render={()=>this.props.currentUser ? (<Redirect to='/'/>) : (<SignInPage/>)}
+
+           />
         </Switch>
       </div>
     )
   }
   
 }
+
+const mapStateToProps = ({user}) =>({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user =>dispatch(setCurrentUser(user))
